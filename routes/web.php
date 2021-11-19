5d32912c62a490490a98f9b49239d92f4f842004
Unesio Severino <unesio.private@gmail.com>
2021-11-19 02:45:41 +0200

    Criação de duas Routas.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-        return view('index');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
