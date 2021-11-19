a8e6ca40fd5cce6ab20a74625edcd4744380c107
Unesio Severino <unesio.private@gmail.com>
2021-11-19 02:46:36 +0200

    Alteracao no template de dialogo

--- resources/views/dialogs/template.blade.php
+++ resources/views/dialogs/template.blade.php
@@ -3,15 +3,15 @@
     <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
        <div class="modal-content">
           <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+             <h5 class="modal-title" id="exampleModalLabel">Deseja Sair?</h5>
              <button class="close" type="button" data-dismiss="modal" aria-label="Close">
              <span aria-hidden="true">×</span>
              </button>
           </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-body">Se confirma o encerramento clica em sair.</div>
           <div class="modal-footer">
-             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-             <a class="btn btn-primary" href="login.html">Logout</a>
+             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+             <a class="btn btn-primary" href="login.html">Confirma</a>
           </div>
        </div>
     </div>
