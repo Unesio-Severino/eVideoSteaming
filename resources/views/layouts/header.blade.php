27a5e6406cee9eb57777f5055947d5d2d84c9488
Unesio Severino <unesio.private@gmail.com>
2021-11-19 02:47:16 +0200

    Alteracoes no header.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,7 +20,7 @@
        <li class="nav-item mx-1">
           <a class="nav-link" href="upload.html">
           <i class="fas fa-plus-circle fa-fw"></i>
-          Upload Video
+          Enviar Video
           </a>
        </li>
        <li class="nav-item dropdown no-arrow mx-1">
@@ -29,10 +29,10 @@
           <span class="badge badge-danger">9+</span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-edit "></i> &nbsp; Action</a>
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-headphones-alt "></i> &nbsp; Another action</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-edit "></i> &nbsp; Acção</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-headphones-alt "></i> &nbsp; Outras acções</a>
              <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-star "></i> &nbsp; Something else here</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-star "></i> &nbsp; Alguma coisa aqui</a>
           </div>
        </li>
        <li class="nav-item dropdown no-arrow mx-1">
@@ -41,23 +41,23 @@
           <span class="badge badge-success">7</span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-edit "></i> &nbsp; Action</a>
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-headphones-alt "></i> &nbsp; Another action</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-edit "></i> &nbsp; Acção</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-headphones-alt "></i> &nbsp; Outras acções</a>
              <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-star "></i> &nbsp; Something else here</a>
+             <a class="dropdown-item" href="#"><i class="fas fa-fw fa-star "></i> &nbsp; Alguma coisa aqui</a>
           </div>
        </li>
        <li class="nav-item dropdown no-arrow osahan-right-navbar-user">
           <a class="nav-link dropdown-toggle user-dropdown-link" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <img alt="Avatar" src="{{ asset('assets/img/user.png') }}">
-          Osahan
+          Username
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-             <a class="dropdown-item" href="account.html"><i class="fas fa-fw fa-user-circle"></i> &nbsp; My Account</a>
-             <a class="dropdown-item" href="subscriptions.html"><i class="fas fa-fw fa-video"></i> &nbsp; Subscriptions</a>
-             <a class="dropdown-item" href="settings.html"><i class="fas fa-fw fa-cog"></i> &nbsp; Settings</a>
+             <a class="dropdown-item" href="account.html"><i class="fas fa-fw fa-user-circle"></i> &nbsp; Minha Conta</a>
+             <a class="dropdown-item" href="subscriptions.html"><i class="fas fa-fw fa-video"></i> &nbsp; Subscrições</a>
+             <a class="dropdown-item" href="settings.html"><i class="fas fa-fw fa-cog"></i> &nbsp; Configurações</a>
              <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal"><i class="fas fa-fw fa-sign-out-alt"></i> &nbsp; Logout</a>
+             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal"><i class="fas fa-fw fa-sign-out-alt"></i> &nbsp; Sair</a>
           </div>
        </li>
     </ul>
