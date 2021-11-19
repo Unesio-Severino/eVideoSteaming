3352f7051f5851d24e8a43e32616ae2eb53059dd
Unesio Severino <unesio.private@gmail.com>
2021-11-19 20:59:55 +0200

    Integrando o auth-layout e auth-slide-show.

---
+++ resources/views/layouts/auth-slide-show.blade.php
@@ -0,0 +1,27 @@
+<div class="col-md-7">
+    <div class="login-main-right bg-white p-5 mt-5 mb-5">
+       <div class="owl-carousel owl-carousel-login">
+          <div class="item">
+             <div class="carousel-login-card text-center">
+                <img src="{{ asset('assets/img/login.png') }}" class="img-fluid" alt="LOGO">
+                <h5 class="mt-5 mb-3">​Assista videos gratuitamente</h5>
+                <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+             </div>
+          </div>
+          <div class="item">
+             <div class="carousel-login-card text-center">
+                <img src="{{ asset('assets/img/login.png') }}" class="img-fluid" alt="LOGO">
+                <h5 class="mt-5 mb-3">Encontre os melhores cursos online</h5>
+                <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+             </div>
+          </div>
+          <div class="item">
+             <div class="carousel-login-card text-center">
+                <img src="{{ asset('assets/img/login.png') }}" class="img-fluid" alt="LOGO">
+                <h5 class="mt-5 mb-3">Videos para as todas areas</h5>
+                <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
