64b3bdda93f99585fed1330457fa2b612f0ea085
Unesio Severino <unesio.private@gmail.com>
2021-11-20 02:29:48 +0200

    Criacao do handler responsavel por upload

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Traits;\UploadFiles;
 
 class ProfileController extends Controller
 {
+    use UploadFiles;
+
     public function updateProfile(Request $request)
     {
         //Metodos para validar campos
@@ -17,19 +20,50 @@ class ProfileController extends Controller
             'telefone'   => 'nullable|min:9',
             'gender'     => 'nullable|min:1|max:1',
             'address'    => 'nullable|min:1|max:150',
-        ]);
+        ]);
 
-        //Metodos para atuzalizar o user
-        $user = User::find (auth()->id());
+        //Metodos para atualizar o user
+        $user = User::find (auth()->id());
 
-        $user->name       = $request->name;
-        $user->telefone   = $request->telefone;
-        $user->gender     = $request->gender;
-        $user->address    = $request->address;
+        $user = this->handleAvatarUpload($request, $user)
 
-        $user->save();
+        $user->name       = $request->name;
+        $user->telefone   = $request->telefone;
+        $user->gender     = $request->gender;
+        $user->address    = $request->address;
 
-        return redirect()->back()->with(['message' => 'Perfil Actualizado com sucesso.']);
+        $user->save();
+
+        return redirect()->back()->with(['message' => 'Perfil Atualizado com sucesso.']);
+    }
+
+    private function handleAvatarUpload (Request $request, User $user)
+    {
+        $avatarInputName = 'avatar'
+        // Check if a profile image has been uploaded
+        if ($request->has(' avatar')) {
+            // Get image file
+            $image = $request->file(' avatar');
+            // Make a image name based on user name and current timestamp
+            $name = Str::slug($request->input('name')).'_'.time();
+            // Define folder path
+            $folder = '/uploads/images/';
+            // Make a file path where image will be stored [ folder path + file name + file extension]
+            $filePath = $folder . $name. '.' . $image->getClientOriginalExtension();
+            // Upload image
+            $this->uploadImage($image, $folder, 'public', $name);
+
+            //verifica se existe ficheiros de fotos relacionadas se nao ele apaga tudo desnecessário e apaga image.
+                if ($user->avatar) {
+                    $this->deleteImage($folder, 'public', explode('/', $user->avatar) [3]);
+                }
+
+            // Set user profile image path in database to filePath
+            $user->avatar = $filePath;
+        }
+
+        return $user;
+
     }
 
 }
