236dafb5149991e9c6d2a7be09827f456a429d7f
Unesio Severino <unesio.private@gmail.com>
2021-11-21 19:05:38 +0200

    Adicao da coluna String "avatar"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -14,18 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('telefone', length: 9)->nullable();
-            $table->string('gender', length: 10)->nullable();
-            $table->string('address', length: 150)->nullable();
-            $table->string('avatar', length: 150)->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('telefone', length: 9)->nullable();
+            $table->string('gender', length: 10)->nullable();
+            $table->string('address', length: 150)->nullable();
+            $table->string('avatar', length: 150)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -35,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users');
     }
 }
