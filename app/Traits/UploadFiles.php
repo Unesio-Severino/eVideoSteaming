a3c4b863b028deb4c6fe3183ccd978dcc806e3e2
Unesio Severino <unesio.private@gmail.com>
2021-11-21 19:06:29 +0200

    Criacao da Funcao de Upload e Replace new foto

--- app/Traits/UploadFiles.php
+++ app/Traits/UploadFiles.php
@@ -1,29 +1,23 @@
-<?php
+<?php 
 
 namespace App\Traits;
 
 use Illuminate\Support\Str;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-
-use Storage;
 
 trait UploadFiles
 {
-
-public function uploadImage($uploadedFile, $folder = null, $disk = 'public', $filename = null)
-
+    public function uploadImage($uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
-        $name = !is_null($filename) ? $filename : Str::random(25);
+        $name = !is_null($filename) ? $filename : Str::random(25);
 
-        $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
+        $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
 
-        return $file;
+        return $file;
     }
 
-public function deleteImage($folder = null, $disk = 'public', $filename = null)
+    public function deleteImage($folder = null, $disk = 'public', $filename = null)
     {
-        Storage::disk($disk)->delete($folder.$filename);
+        Storage::disk($disk)->delete($folder.$filename);
     }
-
 }
